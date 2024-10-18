0743c702beb6e09629a3f8a2310597e8c32a53d4
ipekersanli <95385327+ipekersanli@users.noreply.github.com>
2024-10-18 14:17:36 +0100

    week 2 activities

---
+++ src/dataprep/dataprep.py
@@ -0,0 +1,10 @@
+import pathlib
+import pandas as pd
+from pathlib import Path
+if __name__ == '__main__':
+    csv_file = Path(__file__).parent.parent.joinpath('tutorialpkg/data','paralympics_events_raw.csv')
+    excel_file = Path(__file__).parent.parent.joinpath('tutorialpkg/data','paralympics_all_raw.xlsx')
+
+    csv_df = pd.read_csv(csv_file)
+    excel_df = pd.read_excel(excel_file)
+    csv_df_2 = pd.read_csv(excel_file, sheetname = "medal_standings")
--- src/tutorialpkg/mypkg1/mymodule1.py
+++ src/tutorialpkg/mypkg1/mymodule1.py
@@ -1,3 +1,13 @@
+import pathlib
+from pathlib import Path
+from importlib.resources import files
+from tutorialpkg.mypkg2.mymodule2_1 import calculate_area_of_circle
+from tutorialpkg.mypkg2.mymodule2_2 import fetch_user_data
+
+paralympics_datafile_csv = files("tutorialpkg.data").joinpath("paralympics_events_prepared.csv")
+
+
+
 mock_database = {
     1: {'name': 'Alice', 'email': 'alice@example.com', 'age': 30},
     42: {'name': 'Bob', 'email': 'bob@example.com', 'age': 45},
@@ -14,3 +24,9 @@ if __name__ == '__main__':
     print(fetch_user_data(42, mock_database))
 
     # Locate the data file `paralmpics_raw.csv` relative to this file using pathlib.Path. Prove it exists.
+    csv_file = Path(__file__).parent.parent.joinpath('data','paralympics_events_raw.csv')
+
+    if csv_file.exists():
+        print(f"CSV file found: {csv_file}")
+    else:
+        print("CSV file not found.")
